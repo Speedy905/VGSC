15ed9e158f7e483d0f2e81a206bf1562d78a4d37
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2021-05-05 22:01:32 -0400

    Add options

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@
 # Why? I was curious to see if I could make this work
 # This will follow the previous assignment's structure (At least, for the most part)
 
+
 echo "--------------------------------------------------------------"
 echo "Welcome to the Video Game Storage Creator, rewritten in bash!"
 echo " __   _____ ___  ___" 
@@ -12,3 +13,40 @@ echo " \ \ / / __/ __|/ __|"
 echo "  \ V / (_ \__ \ (__ "
 echo "   \_/ \___|___/\___|"
 echo "--------------------------------------------------------------"
+
+Displaychoices(){
+    echo "--------------------------------"
+    echo "Choose the following options:"
+    echo "Start to Start the Creation"
+    echo "Help to display Help/Instructions"
+    echo "Quit to quit the creation"
+    echo "Credits to view the credits"
+    echo "--------------------------------"
+}
+
+while :
+do
+    Displaychoices
+    read -p "Enter your choice (Case sensitive): " ans
+    case $ans in
+        "Start")
+            echo "S"
+            break
+            ;;
+        "Help")
+            echo "H"
+            break
+            ;;
+        "Quit")
+            echo "Q"
+            break
+            ;;
+        "Credits")
+            echo "C"
+            break
+            ;;
+        *)
+            echo "Invalid choice"
+            ;;
+    esac
+done
