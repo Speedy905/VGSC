b65f5aec430ac27bff51c42df83e22073766a642
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2021-05-05 21:07:44 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Antonio Karlo Mijares
+# Video Game Storage Creator rewritten in bash
+# Why? I was curious to see if I could make this work
+# This will follow the previous assignment's structure (At least, for the most part)
+
+echo "--------------------------------------------------------------"
+echo "Welcome to the Video Game Storage Creator, rewritten in bash!"
+echo " __   _____ ___  ___" 
+echo " \ \ / / __/ __|/ __|"
+echo "  \ V / (_ \__ \ (__ "
+echo "   \_/ \___|___/\___|"
+echo "--------------------------------------------------------------"
