6f7edcf35cf74d25a6dd841d47afb5243b8b397f
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2021-05-13 12:04:18 -0400

    Sub genre check

--- genre.sh
+++ genre.sh
@@ -9,6 +9,39 @@ displaymainGenre(){
     cat dictionaries/main-genres.txt
 }
 
+subgenre(){
+    sub="${*,,}"
+    clear
+    subfile=dictionaries/sub-genres/$sub.txt
+    echo "What subgenre does it belong to?"
+    cat "$subfile"
+    echo "Enter your choice, case sensitive (w/o quotations)"
+    echo "Or type 'Go back' to go back to the previous menu"
+    echo "Or type 'Unknown' if not shown"
+    read -p "Response: " subans
+    
+    case $subans in
+        "Go back")
+            return 1
+            ;;
+        "Unknown")
+            echo "Main genre: $*" >> $filename
+            echo "Sub genre: $subans" >> $filename
+            return 0
+            ;;
+        *)
+            if grep -Fxq "$subans" "$subfile" ; then
+                echo "Main genre: $*" >> $filename
+                echo "Sub genre: $subans" >> $filename
+                return 0
+            else
+                echo "Sub genre not found."
+                sleep 1
+                return 1
+            fi
+            ;;
+    esac
+}
 
 genremenu(){
     while :
@@ -17,9 +50,16 @@ genremenu(){
         read -p "Enter name: " ans
         if grep -Fxq "$ans" dictionaries/main-genres.txt; then
             if [ $ans == "Unknown" ]; then
-                echo "$ans" >> $filename
+                echo "Main genre: $ans" >> $filename
                 echo "-----------------------------" >> $filename
                 source ./inventory.sh
+            else
+                if subgenre "$ans"; then
+                    echo "-----------------------------" >> $filename
+                    source ./inventory.sh
+                else
+                    continue
+                fi
             fi
         else
             echo "Invalid answer. Please try again."
